a10f6bfecda093f6395bfddb121f136a39fa1dcb
Levent Akdogan <lakdogan@1-A-28.42heilbronn.de>
2025-05-28 21:47:04 +0200

    ADD: gc_safe_strjoin.c

--- garbage_collector/inc/garbage_collector.h
+++ garbage_collector/inc/garbage_collector.h
@@ -6,7 +6,7 @@
 /*   By: lakdogan <lakdogan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/25 17:49:25 by lakdogan          #+#    #+#             */
-/*   Updated: 2025/05/27 18:04:16 by lakdogan         ###   ########.fr       */
+/*   Updated: 2025/05/28 21:45:39 by lakdogan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -453,7 +453,8 @@ char					**gc_str_array_create(t_gc *gc, size_t size);
 char					***gc_str_matrix_create(t_gc *gc, size_t rows,
 							size_t cols);
 char					***gc_str_matrix_dup(t_gc *gc, char ***src);
-
+char					*gc_safe_strjoin(t_gc *gc, const char *s1,
+							const char *s2);
 // 4. Integer array functions
 
 int						*gc_int_array_create(t_gc *gc, size_t size);
